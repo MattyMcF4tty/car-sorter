5b0e8bd7877b179b8c92c376a62c859d83ec6468
MattyMcF4tty <matthias.r.kristensen@gmail.com>
2023-04-12 00:04:57 +0200

    Switched from using list to using table on CarList and Car

--- components/car_list.tsx
+++ components/car_list.tsx
@@ -11,11 +11,11 @@ interface CarItemProps {
 const CarItem = ({ numberplate, location, powerPercent }: CarItemProps) => {
 
   return (
-    <li className='flex flex-row even:bg-slate-200'>
-      <p>{ numberplate }</p>
-      <p className='mx-4'>{ location.lat }, { location.lng }</p>
-      <p>{ powerPercent }</p>
-    </li>
+    <tr className='even:bg-slate-200'>
+      <td>{ numberplate }</td>
+      <td>{ location.lat }, { location.lng }</td>
+      <td>{ powerPercent }</td>
+    </tr>
   )
 }
 
@@ -30,15 +30,26 @@ interface CarListProps {
 const CarList = ({ cars }: CarListProps) => {
 
   return (
-    <ul>
-      {cars && cars.map((car) => (
-        <CarItem
-          numberplate={car.numberPlate}
-          location={car.location}
-          powerPercent={car.powerPercent}
-        />
-      ))}
-    </ul>
+    <table>
+      <thead>
+        <tr>
+          <th className='font-semibold'>Numberplate</th>
+          <th className='font-semibold mx-4'>Location</th>
+          <th className='font-semibold'>Battery Percentage</th>
+        </tr>
+      </thead>
+      <tbody>
+        {cars && cars.map((car) => (
+          <CarItem
+            key={car.numberPlate}
+            numberplate={car.numberPlate}
+            location={car.location}
+            powerPercent={car.powerPercent}
+          />
+        ))}
+      </tbody>
+    </table>
   )
 }
 export default CarList;
+
