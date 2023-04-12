bdace2801c02684613f589920997c5928a2257fc
Carloslund <carloslundrodriguez@gmail.com>
2023-04-12 11:08:08 +0200

    navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,17 +12,17 @@ const Navbar = () => {
   };
 
   return (
-    <div className="w-[7rem] h-[100vh] bg-green-500 fixed flex flex-col">
-      <img src="GreenLogo.png" alt="" className="w-1/2" />
-      <div className="w-full mt-36">
+    <div className="w-[7rem] h-[100vh] bg-green-500 fixed flex flex-col z-20">
+      <img src="GreenLogo.png" alt="" className="w-3/5" />
+      <div className="w-full mt-40">
         <button
-          className="w-full h-10 bg-green-800 text-white"
+          className="w-[120%] h-10 bg-green-800 text-white translate-x-[-17%] hover:translate-x-0 duration-200 hover:pr-[45.55px]"
           onClick={listPage}
         >
           List
         </button>
         <button
-          className="w-full h-10 bg-green-800 text-white"
+          className="w-[120%] h-10 bg-green-800 text-white translate-x-[-17%] hover:translate-x-0 duration-200 hover:pr-[45.55px]"
           onClick={mapPage}
         >
           Map
