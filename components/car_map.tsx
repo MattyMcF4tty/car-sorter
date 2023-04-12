be0da0bce23c8af25218910d194e9e7dc066f8ce
MattyMcF4tty <matthias.r.kristensen@gmail.com>
2023-04-12 10:39:26 +0200

    Google maps dev [stable]
    No possible yes to add cars to map

--- components/car_map.tsx
+++ components/car_map.tsx
@@ -1,26 +1,59 @@
-import { GoogleMap, LoadScript, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, Marker, useJsApiLoader } from "@react-google-maps/api";
 
-const CarMap = () => {
+interface CarMapProps {
+    cars: {
+        numberPlate: number;
+        location: { lat: number; lng: number };
+        powerPercent: number;
+    }[];
+}
+
+const CarMap = ({ cars }: CarMapProps) => {
     const center = { lat: 0, lng: 0 };
 
     const { isLoaded, loadError } = useJsApiLoader({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-    libraries: ["places"],
+        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+        libraries: ["places"],
     });
 
     if (loadError) {
-        return <div>Error loading Google Maps API: {loadError.message}</div>;
-    }
-
-    if (isLoaded) {
         return (
-        <div className='w-80 h-80'>
-            <GoogleMap zoom={15} center={center} />
-        </div>
+          <div>
+            <p>Error loading Google maps</p>
+          </div>
         );
-    }
-
-    return <div>Loading Google Maps API...</div>;
+      } else if (!isLoaded) {
+        return (
+          <div>
+            <p>Loading Google maps...</p>
+          </div>
+        );
+      } else {
+          return (
+              <div className="w-full h-full">
+                  <GoogleMap
+                  center={center}
+                  zoom={5}
+                  mapContainerStyle={{
+                      height: "100%",
+                      width: "100%",
+                  }}
+                  options={{
+                      fullscreenControl: false,
+                      zoomControl: false,
+                      streetViewControl: false,
+                  }}
+                  >
+                    {cars &&
+                        cars.map((car) => (
+                            <Marker 
+                            position={car.location}
+                            />
+                        ))}
+                  </GoogleMap>
+              </div>
+          );
+      }
 };
 
 export default CarMap;
--- pages/map.tsx
+++ pages/map.tsx
@@ -1,11 +1,12 @@
+import { useState } from "react";
 import CarMap from "../components/car_map";
 
 const MapPage = () => {
+    const [cars, setCars] = useState<{ numberPlate: number; location: { lat: number; lng: number }; powerPercent: number }[]>([]);
 
     return (
-        <div className="w-80 h-80">
-            <h1>Maps</h1>
-            <CarMap />
+        <div className="w-[calc(100vw-7rem)] h-[100vh]">
+            <CarMap cars={cars}/>
         </div>
     )
 }
